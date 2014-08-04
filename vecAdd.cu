f0fcb62ceed92d1105c96a806d9687c54eb56f7a
Kris Frye <kfrye@pdx.edu>
2014-08-03 19:51:45 -0700

    Got kernel working! Includes comments

--- vecAdd.cu
+++ vecAdd.cu
@@ -1,18 +1,21 @@
+/* 
+ * Kristina Frye
+ * Dean Nida
+ * August 7, 2014
+ * HW 4, #1
+ */
+
 #include <stdio.h>
 
 #define numAtoms 100
 #define BLOCK 32
 
-typedef struct {
-  	float x;
-  	float y;
-  	float z;
-  	float w;
- }atom;
+struct atom {
+   float x; float y; float z; float w;
+ } a;
 
-__constant__ atom atominfo[numAtoms];
+__constant__ struct atom atominfo[numAtoms];
 
-/*error checking from D&W*/
 void errorChecking(cudaError_t err, int line) {
     if (err != cudaSuccess){
         printf(" %s in %s at line %d\n", cudaGetErrorString(err),
@@ -21,91 +24,85 @@ void errorChecking(cudaError_t err, int line) {
     }
 }
 
+/* A function for printing out our data */
 void print_vector(float *array, int n) {
     int i;
-    for (i=0; i<n; i++)
+    for (i=0; i<n*n; i++)
         printf("%0.0f ", array[i]);
     printf("\n");
 }
 
+/* Cuda Kernel function */
 __global__ void DCSv1(float *energygrid, float *gridspacing, int *numatoms){
 
+   // Get thread addresses
    int xindex = blockDim.x * blockIdx.x + threadIdx.x;
    int yindex = blockDim.y * blockIdx.y + threadIdx.y;
    int outaddr = yindex * numAtoms + xindex;
-   //int num = *numatoms;   
-   //if(xindex > num || yindex > num) return;
-/*
+   int num = *numatoms;  
+   int gridsp = *gridspacing; 
+
+   // Check for thread boundries
+   if(xindex > num - 1 || yindex > num - 1) return;
+
+   // Start algorithm from 12.5 in book
    float curenergy = energygrid[outaddr];
-   float coorx = (*gridspacing) * xindex;
-   float coory = (*gridspacing) * yindex;
+   float coorx = gridsp * xindex;
+   float coory = gridsp * yindex;
    int atomid;
    float energyval=0.0f;
-*/
-/*
-   for (atomid=0; atomid<(*numatoms); atomid++) {
+
+   for (atomid=0; atomid<num; atomid++) {
       float dx = coorx - atominfo[atomid].x;
       float dy = coory - atominfo[atomid].y;
       energyval += atominfo[atomid].w*sqrtf(dx*dx + dy*dy + atominfo[atomid].z);
    }
    energygrid[outaddr] = curenergy + energyval;
-*/
-   energygrid[outaddr] = threadIdx.y; 
 }
 
-
-void launch_DSCv1(float * energyGrid, int boxDim, atom * molecule, float gridDist){
+/* Launches cuda kernel */
+void launch_DSCv1(float * energyGrid, int boxDim, atom * molecule, 
+       float gridDist){
     float *grid_dev, *spacing_dev, *spacing; 
     int *numatoms_dev;
     int *num;
     int allocateSize = sizeof(float) * boxDim*boxDim*boxDim;
 
-    printf("\nInside the launch function.\n");
-    print_vector(energyGrid, boxDim);
-
+    // Malloc memory for local host pointer variables used for copying data
+    // to the device
     spacing = (float*)malloc(sizeof(float));
     *spacing = gridDist;
     num = (int*)malloc(sizeof(int));
     *num = numAtoms;
- 
+
+    // Malloc memory on the device
     errorChecking( cudaMalloc((void **) &grid_dev, allocateSize), __LINE__);
     errorChecking( cudaMalloc((void **) &spacing_dev, sizeof(float)), __LINE__);
     errorChecking( cudaMalloc((void **) &numatoms_dev, sizeof(int)), __LINE__);
-/* 
-    char *string_dev;
-    char string [5] = {"abcd"};
-    int length = 5;
-    
-    int size = allocateSize;
-    
-    float *h_A = (float *)malloc(size);
-    float *d_A = NULL;
-    err = cudaMalloc((void **)&d_A, size);
-    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-    
-    errorChecking(err, __LINE__);
-    errorChecking(err, __LINE__);
-    
-    errorChecking(cudaMalloc((void **) &string_dev, sizeof(char) * length), __LINE__);
-    errorChecking(cudaMemcpy(string_dev, string, sizeof(char) * length, cudaMemcpyHostToDevice), __LINE__);
-*/
 
-    errorChecking(cudaMemcpy(grid_dev, energyGrid, allocateSize, cudaMemcpyHostToDevice), __LINE__);
-    errorChecking(cudaMemcpy(spacing_dev, spacing, sizeof(float), cudaMemcpyHostToDevice), __LINE__);
-    errorChecking(cudaMemcpy(numatoms_dev, num, sizeof(int), cudaMemcpyHostToDevice), __LINE__); 
-    errorChecking(cudaMemcpyToSymbol(atominfo, &molecule, sizeof(atom)*numAtoms, 0, cudaMemcpyHostToDevice), __LINE__);
+    // Copy data to the device 
+    errorChecking(cudaMemcpy(grid_dev, energyGrid, allocateSize, 
+       cudaMemcpyHostToDevice), __LINE__);
+    errorChecking(cudaMemcpy(spacing_dev, spacing, sizeof(float), 
+       cudaMemcpyHostToDevice), __LINE__);
+    errorChecking(cudaMemcpy(numatoms_dev, num, sizeof(int), 
+       cudaMemcpyHostToDevice), __LINE__); 
+    errorChecking(cudaMemcpyToSymbol(atominfo, molecule, 
+      sizeof(atom)*numAtoms, 0, cudaMemcpyHostToDevice), __LINE__);
 
+    // Determine size of blocks and grid on device
     int numWidth = ceil((float)boxDim/BLOCK);
     int numHeight = ceil((float)boxDim/BLOCK);
-    printf("numWidth: %d, numHeight: %d\n", numWidth, numHeight);
     dim3 dimGrid(numWidth, numHeight); 
     dim3 dimBlock(BLOCK, BLOCK);
-    printf("Grid.y: %d, Block.y: %d\n", dimGrid.y, dimBlock.y);
+
+    // Call the kernel
     DCSv1<<<dimGrid, dimBlock, 0>>>(grid_dev, spacing_dev, numatoms_dev);
     errorChecking(cudaGetLastError(), __LINE__);
    
     // Step 4: Retrieve the results
-    errorChecking( cudaMemcpy(energyGrid, grid_dev, allocateSize, cudaMemcpyDeviceToHost), __LINE__);
+    errorChecking( cudaMemcpy(energyGrid, grid_dev, allocateSize, 
+       cudaMemcpyDeviceToHost), __LINE__);
 
     // Step 5: Free device memory
     cudaFree(&grid_dev);
@@ -117,28 +114,25 @@ int main(void) {
     atom molecule[numAtoms];   
 
     int boxDim = numAtoms;
-    float * energyGrid;
+    float *energyGrid;
     float gridDist = 1; 
-    energyGrid = (float * ) malloc( boxDim*boxDim*boxDim  * sizeof(float)); 
+    energyGrid = (float *) malloc(boxDim*boxDim*boxDim*sizeof(float)); 
 
     for (int i = 0; i <  boxDim*boxDim*boxDim ; ++i){
        energyGrid[i] = 1;
     }
    
-    printf("Energy grid before kernel:\n");
-    print_vector(energyGrid, boxDim); 
-
     for( int i = 0; i < numAtoms; ++i){
       molecule[i].x = i; 
       molecule[i].y = i; 
       molecule[i].z = i; 
       molecule[i].w = i;
-    } 
+    }
 
     launch_DSCv1(energyGrid, boxDim, molecule, gridDist);
-
+/*
     printf("\nEnergy grid after kernel:\n");
     print_vector(energyGrid, boxDim);
-
+*/
     return 0;
 }
